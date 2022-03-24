70cbb2beddc22f9e5746684e8794594a62e63a66
Kleber Silva <kleber.swf@gmail.com>
2022-03-23 22:59:42 -0300

    contents and panel as one

--- src/components/contents/ContentsPanel.svelte
+++ src/components/contents/ContentsPanel.svelte
@@ -1,12 +1,37 @@
 <script lang="ts">
 	import type { TreeNodeModel } from '../../model/model';
-	import Panel from '../../ui/Panel.svelte';
 	import TreeNode from './TreeNode.svelte';
 
 	export let nodes: TreeNodeModel;
 	export let selected: string;
 </script>
 
-<Panel title="Contents">
-	<TreeNode {...nodes} on:select {selected} />
-</Panel>
+<div class="contents-panel">
+	<div class="title">Contents</div>
+	<div class="content">
+		<TreeNode {...nodes} on:select {selected} />
+	</div>
+</div>
+
+<style lang="scss" scoped>
+	@import 'variables.scss';
+
+	.contents-panel {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+
+		.title {
+			background-color: $dark-background;
+			padding: 4px 8px;
+		}
+
+		.content {
+			padding: 4px 8px;
+			height: 100%;
+			overflow: auto;
+			flex: 1;
+		}
+	}
+</style>

72568b92e400a4df3858754457b939d6047955ba
Kleber Silva <kleber@jogoglobal.com>
2022-03-31 13:33:10 -0300

    Preview Area: workspace

--- src/components/preview/FrameSelection.svelte
+++ src/components/preview/FrameSelection.svelte
@@ -2,15 +2,14 @@
 	import type { SelectionModel } from '../../model/app.model';
 
 	export let selection: SelectionModel;
-	export let zoom: number;
+	export let scale: number;
 
 	$: frame = selection.frame?.frame;
-	$: labelMargin = selection.frame?.frame.y < 12 ? { left: 10, top: 3 } : { left: 0, top: -27 };
 </script>
 
 {#if frame}
 	<div class="rect" style="top:{frame.y}px; left:{frame.x}px; width:{frame.w}px; height:{frame.h}px">
-		<div class="name" style="zoom:{1 / zoom}; margin:{labelMargin.top}px 0 0 {labelMargin.left}px">
+		<div class="name" style="zoom:{1 / scale}">
 			{selection.path}
 		</div>
 	</div>
@@ -33,5 +32,6 @@
 		text-shadow: 0 0 2px rgba(255, 255, 255, 0.8), 0 0 4px #0df, 0 0 8px #0df, 0 0 12px #0ff;
 		overflow: visible;
 		white-space: nowrap;
+		margin-top: -27px;
 	}
 </style>
--- src/components/preview/PreviewArea.svelte
+++ src/components/preview/PreviewArea.svelte
@@ -6,32 +6,44 @@
 	export let imgSrc: string;
 	export let selection: SelectionModel;
 
-	let zoom = 1;
-	let container: HTMLDivElement;
+	let scale = 1;
+	let panning = false;
+	let containerWidth: number;
+	let containerHeight: number;
+
+	let root: HTMLDivElement;
+	let image: HTMLImageElement;
 
 	const dispatch = createEventDispatcher();
 
 	function selectFrame(e: MouseEvent) {
-		dispatch('touch', { x: e.offsetX / zoom, y: e.offsetY / zoom });
+		dispatch('touch', { x: e.offsetX, y: e.offsetY });
+	}
+
+	function onImageLoded(e: Event) {
+		image = e.target as HTMLImageElement;
+		if (image.width > image.height) {
+			scale = root.clientWidth / image.width;
+		} else {
+			scale = root.clientHeight / image.height;
+		}
+		root.addEventListener('scroll', centerImage);
+		root.scrollTo(1, 1);
+	}
+
+	function centerImage() {
+		const left = (root.scrollWidth - root.clientWidth) * 0.5;
+		const top = (root.scrollHeight - root.clientHeight) * 0.5;
+		root.scrollTo({ top, left, behavior: 'smooth' });
+		root.removeEventListener('scroll', centerImage);
 	}
 
 	function onMouseWheel(e: WheelEvent) {
 		if (!e.ctrlKey) return;
-		zoom = Math.max(0.1, Math.min(5, zoom - e.deltaY * 0.001));
+		scale = Math.max(0.1, Math.min(5, scale - e.deltaY * 0.001));
 		e.preventDefault();
 		e.stopImmediatePropagation();
 	}
-
-	function onImageLoded(e: Event) {
-		const img = e.target as HTMLImageElement;
-		if (img.width > img.height) {
-			zoom = container.clientHeight / img.width;
-		} else {
-			zoom = container.clientHeight / img.height;
-		}
-	}
-
-	let panning = false;
 
 	function onMouseDown(e: MouseEvent) {
 		panning = e.button === 1;
@@ -41,18 +53,28 @@
 
 	function onMouseMove(e: MouseEvent) {
 		if (!panning) return; // middle button
-		container.scrollTop -= e.movementY;
-		container.scrollLeft -= e.movementX;
+		root.scrollTop -= e.movementY;
+		root.scrollLeft -= e.movementX;
 		e.preventDefault();
+	}
+
+	$: {
+		if (image) {
+			containerWidth = scale * (image.clientWidth + 200);
+			containerHeight = scale * (image?.clientHeight + 200);
+		} else {
+			containerWidth = 200;
+			containerHeight = 200;
+		}
 	}
 </script>
 
-<div class="preview" bind:this={container} on:wheel={onMouseWheel} on:mousedown={onMouseDown} on:mousemove={onMouseMove}>
-	<div class="image-container">
-		<div class="internal" style="zoom:{zoom}">
+<div class="preview" bind:this={root} on:wheel={onMouseWheel} on:mousedown={onMouseDown} on:mousemove={onMouseMove}>
+	<div class="image-container" style="min-width:{containerWidth}px; min-height:{containerHeight}px">
+		<div class="internal" style="transform:scale({scale})">
 			<img src={imgSrc} alt="" on:click={selectFrame} on:load={onImageLoded} />
 			{#if selection}
-				<FrameSelection {selection} {zoom} />
+				<FrameSelection {selection} {scale} />
 			{/if}
 		</div>
 	</div>
@@ -65,23 +87,29 @@
 		user-select: none;
 		// background-image: url('/assets/patterns/checkers.png');
 		background-image: url('/assets/patterns/shadow-checkers.png');
+		width: 100%;
+		height: 100%;
 		min-width: 100%;
 		min-height: 100%;
+		max-width: 100%;
+		max-height: 100%;
 		overflow: auto;
 
 		.image-container {
 			position: relative;
-			padding: 20px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
 			.internal {
-				position: absolute;
-			}
-			img {
-				box-sizing: content-box;
-				-webkit-user-drag: none;
-				user-select: none;
-				-moz-user-select: none;
-				-webkit-user-select: none;
-				-ms-user-select: none;
+				img {
+					transform-origin: center;
+					box-sizing: content-box;
+					-webkit-user-drag: none;
+					user-select: none;
+					-moz-user-select: none;
+					-webkit-user-select: none;
+					-ms-user-select: none;
+				}
 			}
 		}
 	}

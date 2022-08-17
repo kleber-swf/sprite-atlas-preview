1ccf05d2e8e95eb96aa492531c0eee26e531d748
Kleber Silva <kleber@jogoglobal.com>
2022-08-17 17:55:23 -0300

    Added: expand/collapse tree nodes

--- src/components/tree/TreeNode.svelte
+++ src/components/tree/TreeNode.svelte
@@ -13,9 +13,13 @@
 	const dispatch = createEventDispatcher();
 
 	function selectNode(e: MouseEvent) {
-		console.log('select node', path);
-		dispatch('select', path);
 		e.stopImmediatePropagation();
+		dispatch('select', path);
+	}
+
+	function toggleOpen(e: MouseEvent) {
+		e.stopImmediatePropagation();
+		open = !open;
 	}
 
 	let isSelected: boolean;
@@ -28,36 +32,82 @@
 </script>
 
 <div bind:this={element} class="tree-node" class:selected={isSelected} on:click={selectNode}>
-	<div class="node-name" style="padding-left: {indent}px">{name}</div>
-
-	{#if open}
-		<div class="children">
-			{#each children as child (child.path)}
-				<svelte:self {...child} indent={indent + 12} {selected} on:select />
-			{/each}
+	<div class="node-header" style="padding-left: {indent}px">
+		<div class="node-icon" on:click={toggleOpen} class:expanded={open}>
+			<span class="icon" class:visible={children.length}>&#9658;</span>
 		</div>
-	{/if}
+		<span class="node-name">{name}</span>
+	</div>
+
+	<div class="children" class:expanded={open}>
+		{#each children as child (child.path)}
+			<svelte:self {...child} indent={indent + 12} {selected} on:select />
+		{/each}
+	</div>
 </div>
 
 <style lang="scss">
 	@import 'variables';
+
+	$anim-duration: 250ms;
+
 	.tree-node {
-		cursor: pointer;
 		user-select: none;
+		cursor: pointer;
+
+		.node-header {
+			display: flex;
+			justify-content: stretch;
+			align-items: stretch;
+			padding: 4px 0;
+			line-height: 20px;
+
+			.node-icon {
+				display: inline-block;
+				width: 16px;
+				margin-right: 2px;
+				text-align: center;
+				opacity: 0.5;
+				transition: all $anim-duration;
+
+				.icon {
+					visibility: hidden;
+					transform-origin: 50%;
+				}
+
+				&:hover {
+					color: $on-primary;
+					opacity: 1;
+				}
+
+				&.expanded {
+					transform: rotate(90deg);
+				}
+			}
+
+			&:hover {
+				color: $on-primary !important;
+			}
+		}
+
+		.children {
+			overflow: hidden;
+			height: 0;
+
+			&.expanded {
+				height: unset;
+			}
+		}
 
 		&.selected {
-			& > .node-name {
+			& > .node-header {
 				background-color: $primary;
 				color: $on-primary;
 			}
 
-			.children .node-name {
+			.children .node-header {
 				color: rgba($on-primary, 0.6);
 			}
-		}
-
-		.node-name {
-			padding: 4px;
 		}
 	}
 </style>
